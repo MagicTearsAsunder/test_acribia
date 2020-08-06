35e31e5baeb07ed537f346f5fa8d12e6cbe3e3da
Magic Tears Asunder <The.Magic.Tears.Asunder@protonmail.com>
2020-08-06 19:07:53 +0300

    Update settings.py

--- acribia/settings.py
+++ acribia/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'acribia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# TODO
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'YOUR DATABASE NAME',
-        'USER': 'USERNAME',
-        'PASSWORD': 'PASSWORD',
-        'HOST': 'HOST',
-        'PORT': 'PORT',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# TODO
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'YOUR DATABASE NAME',
+#         'USER': 'USERNAME',
+#         'PASSWORD': 'PASSWORD',
+#         'HOST': 'HOST',
+#         'PORT': 'PORT',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
